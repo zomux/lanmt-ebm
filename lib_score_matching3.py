662a1c427a87e2adc44796d5348da40ac097db33
shu <raphael@uaca.com>
2020-02-22 23:19:15 +0000

    -

--- lib_score_matching3.py
+++ lib_score_matching3.py
@@ -93,6 +93,8 @@ class LatentScoreNetwork3(Transformer):
         return score_map
 
     def refine(self, z, x, mask=None, n_steps=50, step_size=0.01):
+        if mask is not None:
+            mask = mask.float()
         with torch.no_grad():
             refined_z = self.compute_delta_inference(x, mask, z).detach()
         for _ in range(n_steps):
