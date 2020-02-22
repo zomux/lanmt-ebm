ae03502df2dbfc0aac4705cb97fbb261fb1d8bc6
shu <raphael@uaca.com>
2020-02-22 23:27:30 +0000

    -

--- lib_score_matching3.py
+++ lib_score_matching3.py
@@ -103,14 +103,14 @@ class LatentScoreNetwork3(Transformer):
                 print((z - refined_z).norm(2).detach().cpu().numpy(),
                       energy.mean().detach().cpu().numpy(),
                       grad.norm(2).detach().cpu().numpy())
-            # z = z - step_size * grad
+            z = z - step_size * grad
             # noise = torch.randn_like(z) * np.sqrt(step_size * 2)
             # z = z + step_size * grad + noise
-            norm = grad.norm(dim=2)
-            max_pos = norm.argmax(1)
+            # norm = grad.norm(dim=2)
+            # max_pos = norm.argmax(1)
             # if norm.max() < 0.5:
             #     break
-            z[torch.arange(z.shape[0]), max_pos] -= step_size * grad[torch.arange(z.shape[0]), max_pos]
+            # z[torch.arange(z.shape[0]), max_pos] += step_size * grad[torch.arange(z.shape[0]), max_pos]
             # print(grad.norm(dim=2))
         if not OPTS.evaluate:
             raise SystemExit
