3c96bf841aad9ec31deb1190688e58b1372520f7
shu <raphael@uaca.com>
2020-02-23 15:19:24 +0000

    -

--- lib_simple_encoders.py
+++ lib_simple_encoders.py
@@ -68,7 +68,7 @@ class DisentangledCrossEncoder(nn.Module):
 class DisentangledCrossEncoderLayer(nn.Module):
 
     def __init__(self, size, n_att_head=8, dropout_ratio=0.1, relative_pos=False):
-        super(DisentangledCrossEncoder, self).__init__()
+        super(DisentangledCrossEncoderLayer, self).__init__()
         self.dropout = nn.Dropout(dropout_ratio)
         self.cross_attention = MultiHeadAttention(size, n_att_head, dropout_ratio=dropout_ratio, relative_pos=relative_pos)
         self.ff_layer = nn.Sequential(nn.Linear(size, size), nn.ReLU())
