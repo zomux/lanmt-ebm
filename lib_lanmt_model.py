3d2c286dd9e916711ef0f6ff651b673e75cbdf7c
shu <raphael@uaca.com>
2020-02-26 23:39:18 +0000

    -

--- lib_lanmt_model.py
+++ lib_lanmt_model.py
@@ -186,7 +186,6 @@ class LANMTModel(Transformer):
         y_lens = z_lens + delta
         converted_vectors = self.convert_length(z, z_mask, y_lens)
         # Create target-side mask
-        import pdb;pdb.set_trace()
         max_len = max(1, y_lens.max().long())
         arange = torch.arange(max_len)
         if torch.cuda.is_available():
