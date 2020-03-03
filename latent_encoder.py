c2003464f2593739f1367b93c647df202ee83ee2
shu <raphael@uaca.com>
2020-03-03 04:11:17 +0000

    -

--- latent_encoder.py
+++ latent_encoder.py
@@ -96,9 +96,9 @@ ap.add_argument("--opt_disentangle", action="store_true")
 
 # Paths
 ap.add_argument("--model_path",
-                default="{}/lanmt.pt".format(DATA_ROOT))
+                default="{}/lacoder.pt".format(DATA_ROOT))
 ap.add_argument("--result_path",
-                default="{}/lanmt.result".format(DATA_ROOT))
+                default="{}/lacoder.result".format(DATA_ROOT))
 OPTS.parse(ap)
 
 
