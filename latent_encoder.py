01b203f3f4263e56f65d46737a0fd5ab9ef02d35
shu <raphael@uaca.com>
2020-03-08 23:28:11 +0000

    -

--- latent_encoder.py
+++ latent_encoder.py
@@ -64,6 +64,9 @@ ap.add_argument("--opt_shard", type=int, default=32)
 ap.add_argument("--opt_longertrain", action="store_true")
 ap.add_argument("--opt_x3longertrain", action="store_true")
 
+# Options for latent encoder
+ap.add_argument("--opt_disentangle", action="store_true")
+
 # Options for LANMT
 ap.add_argument("--opt_latentdim", default=8, type=int, help="dimension of latent variables")
 ap.add_argument("--opt_distill", action="store_true", help="train with knowledge distillation")
