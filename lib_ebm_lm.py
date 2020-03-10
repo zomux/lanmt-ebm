c9a71c37f2166cb10804c666894c070c98e4481f
shu <raphael@uaca.com>
2020-03-10 00:02:52 +0000

    -

--- lib_ebm_lm.py
+++ lib_ebm_lm.py
@@ -106,8 +106,7 @@ class EnergyLanguageModel(Transformer):
 
             # Denosing updating
             norm = (grad - z).norm(dim=2)
-            max_pos = norm[:, :-1].argmax(1)
-            print(max_pos)
+            max_pos = norm[:, 0:-1].argmax(1) + 1
             z[:, max_pos] = grad[:, max_pos]
             # noise = torch.randn_like(z) * np.sqrt(step_size * 2)
             # z = z + step_size * grad + noise
