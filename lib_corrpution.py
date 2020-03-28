206690dd3e01387ba85a02a6edd7f5432ae5c0a5
shu <raphael@uaca.com>
2020-03-28 06:09:43 +0000

    -

--- lib_corrpution.py
+++ lib_corrpution.py
@@ -10,4 +10,6 @@ import torch
 def random_token_corruption(seq, vocab_size, ratio=0.2):
     # seq ~ (Batch, Length)
     nosie_tokens = torch.randint(0, vocab_size + 1, seq.shape)
-    mask = (torch)
+    mask = (torch.rand(seq.shape) > ratio).float()
+    seq = seq * mask + nosie_tokens * (1 - mask)
+    return seq
