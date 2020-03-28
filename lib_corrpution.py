226c30262c58ab837753fe9e8a534f451d447d53
shu <raphael@uaca.com>
2020-03-28 06:06:40 +0000

    -

--- lib_corrpution.py
+++ lib_corrpution.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+
 def random_token_corruption(seq, vocab_size, ratio=0.2):
     # seq ~ (Batch, Length)
-    
+    torch.random.randint
