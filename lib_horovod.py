d8f56b82c0de2dcb75e527e2bfc0506df626b437
shu <raphael@uaca.com>
2020-04-05 05:34:29 +0000

    -

--- lib_horovod.py
+++ lib_horovod.py
@@ -5,7 +5,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from nmtlab.utils import is_root_node
 import importlib
+import torch
 
 def initialize_horovod():
     horovod_installed = importlib.util.find_spec("horovod") is not None
@@ -18,6 +20,7 @@ def initialize_horovod():
     else:
         part_index = 0
         part_num = 1
-    print("Running on {} GPUs".format(part_num))
+    if is_root_node():
+        print("Running on {} GPUs".format(part_num))
     return part_index, part_num
 
