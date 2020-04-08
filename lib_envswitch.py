c7fe5f47f681bd1681dab12cc970d10ee6575aea
shu <raphael@uaca.com>
2020-04-08 05:01:54 +0000

    -

--- lib_envswitch.py
+++ lib_envswitch.py
@@ -5,3 +5,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+class EnvSwitcher(object):
+
+    def __init__(self):
+        self.var_map = {}
+
+    def 
